b7d1a790acb57f745a2cf7745696367380d5f24d
sodogan <itelligence.i.dogan@daimler.com>
2021-05-25 15:00:59 +0300

    minor change

--- lib/result.dart
+++ lib/result.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  //voidCallback can also be used here
   final void Function()? resetHandler;
   final totalScore;
 
   Result({required this.totalScore, required this.resetHandler});
 
-  get finalScore => totalScore;
+  int get finalScore => totalScore;
+
+  //add logic depending on the score
+  String get resultPhrase {
+    return "";
+  }
 
   @override
   Widget build(Object context) {
     return Column(
       children: [
-
-        SizedBox(height: 56,),
+        SizedBox(
+          height: 56,
+        ),
         Container(
           //width: double.infinity,
           alignment: Alignment.center,
           //color: Colors.indigo,
           padding: EdgeInsets.all(16),
           margin: EdgeInsets.all(12.0),
-          child: Padding(padding:EdgeInsets.all(6),   
-          child: Text(
-            "You scored $finalScore points",
-            style: TextStyle(color: Colors.green, fontSize: 24.0),
-            textAlign: TextAlign.center,
-          ),
+          child: Padding(
+            padding: EdgeInsets.all(6),
+            child: Text(
+              "You scored $finalScore points",
+              style: TextStyle(color: Colors.green, fontSize: 24.0),
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
         Container(
@@ -42,8 +50,12 @@ class Result extends StatelessWidget {
             ),
           ),
         ),
-        Image.network(
-            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+        Container(
+          child: Image.network(
+              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(12),
+        ),
       ],
     );
   }
